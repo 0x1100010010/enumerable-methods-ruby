3921f31f823e76f8c74d68a2b57e99b6f7a81c9b
bashforger <bash.forger@gmail.com>
2020-11-19 22:53:21 +0500

    Added my_none? method

--- main.rb
+++ main.rb
@@ -1,13 +1,11 @@
 module Enumerable
-  def my_each
-    for i in self do
-      yield i
-    end
+  def my_each(&block)
+    each(&block)
   end
 
   def my_each_with_index
     i = 0
-    while i < self.length
+    while i < length
       yield(self[i], i)
       i += 1
     end
@@ -16,83 +14,98 @@ module Enumerable
 
   def my_select
     arr = []
-    for i in self do
+    each do |i|
       arr << i if yield i
     end
     arr
   end
 
-  def my_all?( *args )
+  def my_all?(*args, &block)
     # puts "args #{args} #{args[0].class}"
-    return (self.my_select { |i| yield(i) }).length == self.length if block_given?
+    return my_select(&block).length == length if block_given?
+
     unless block_given?
-        return true if args==[]
-        return ((self.my_select { |i| i.class == args[0] }).length == self.length) if args[0].is_a? Class
-        return ((self.my_select {|i| i.match(args[0])}).length == self.length) if args[0].is_a? Regexp
-        return (self.my_select { |i| i==args[0] }).length == self.length if args[0].is_a? Integer
-      end
+      return true if args == []
+      return ((my_select { |i| i.instance_of?(args[0]) }).length == length) if args[0].is_a? Class
+      return ((my_select { |i| i.match(args[0]) }).length == length) if args[0].is_a? Regexp
+      return (my_select { |i| i == args[0] }).length == length if args[0].is_a? Integer
+    end
   end
 
-  # def my_all?(*args)
-  #   (self.my_select { |i| yield(i) }).length == self.length
-  #   unless block_given?
-  #     return puts "empty" if args==[]
-  #     # return puts "thats right" if args.class == (Integer)
-  #     # return puts "A Regexp" if args.is_a?(Regexp)
-  #   end
-  # end
+  def my_any?; end
 
+  def my_none?(*args, &block)
+    return my_select(&block).length.zero? if block_given?
 
-  def my_any?
-  end
-
-  def my_none?
+    unless block_given?
+      return length.zero? if args == []
+      return (my_select { |i| i.instance_of?(args[0]) }).length.zero? if args[0].is_a? Class
+      return (my_select { |i| i == args[0] }).length.zero? if args[0].is_a? Integer
+    end
   end
 
   def my_count
+    puts 'my_count'
   end
 
   def my_map
+    puts 'my_maps'
   end
 
   def my_inject
+    puts 'my_inject'
   end
 
   def multiply_els
+    puts 'multiply_els'
   end
 end
 
-# # 1. my_each
-# puts 'my_each'
-# puts '-------'
-# puts [1, 2, 3].my_each { |elem| print "#{elem + 1} " } # => 2 3 4
-# puts
+# 1. my_each
+puts 'my_each'
+puts '-------'
+puts [1, 2, 3].my_each { |elem| print "#{elem + 1} " } # => 2 3 4
+puts
 
 # 2. my_each_with_index
-# puts 'my_each_with_index'
-# puts '------------------'
-# print([1, 2, 3].each_with_index { |elem, idx| puts "#{elem} : #{idx}" }) # => 1 : 0, 2 : 1, 3 : 2
-# print([1, 2, 3].my_each_with_index { |elem, idx| puts "#{elem} : #{idx}" }) # => 1 : 0, 2 : 1, 3 : 2
-# puts
+puts 'my_each_with_index'
+puts '------------------'
+print([1, 2, 3].each_with_index { |elem, idx| puts "#{elem} : #{idx}" }) # => 1 : 0, 2 : 1, 3 : 2
+print([1, 2, 3].my_each_with_index { |elem, idx| puts "#{elem} : #{idx}" }) # => 1 : 0, 2 : 1, 3 : 2
+puts
 
-# # 3. my_select
-# puts 'my_select'
-# puts '---------'
-# p [1, 2, 3, 8].my_select(&:even?) # => [2, 8]
-# p [0, 2018, 1994, -7].my_select { |n| n > 0 } # => [2018, 1994]
-# p [6, 11, 13].my_select(&:odd?) # => [11, 13]
-# puts
+# 3. my_select
+puts 'my_select'
+puts '---------'
+p [1, 2, 3, 8].my_select(&:even?) # => [2, 8]
+p [0, 2018, 1994, -7].my_select(&:positive?) # => [2018, 1994]
+p [6, 11, 13].my_select(&:odd?) # => [11, 13]
+puts
 
 # 4. my_all? (example test cases)
 puts 'my_all?'
 puts '-------'
 p [3, 5, 7, 11].my_all?(&:odd?) # => true
-p [-8, -9, -6].my_all? { |n| n < 0 } # => true
+p [-8, -9, -6].my_all?(&:negative?) # => true
 p [3, 5, 8, 11].my_all?(&:odd?) # => false
-p [-8, -9, -6, 0].my_all? { |n| n < 0 } # => false
+p [-8, -9, -6, 0].my_all?(&:negative?) # => false
 # test cases required by tse reviewer
 p [1, 2, 3, 4, 5].my_all? # => true
 p [1, 2, 3].my_all?(Integer) # => true
 p %w[dog door rod blade].my_all?(/d/) # => true
 p [1, 1, 1].my_all?(1) # => true
+puts
+
+# 6. my_none? (example test cases)
+puts 'my_none?'
+puts '--------'
+p [3, 5, 7, 11].my_none?(&:even?) # => true
+p %w[sushi pizza burrito].my_none? { |word| word[0] == 'a' } # => true
+p [3, 5, 4, 7, 11].my_none?(&:even?) # => false
+p %w[asparagus sushi pizza apple burrito].my_none? { |word| word[0] == 'a' } # => false
+# test cases required by tse reviewer
+p [1, 2, 3].my_none? # => false
+p [1, 2, 3].my_none?(String) # => true
+p [1, 2, 3, 4, 5].my_none?(2) # => false
+p [1, 2, 3].my_none?(4) # => true
 puts
