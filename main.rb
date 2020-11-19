65d069757a1c37216eb43eeaa6c64e24b75a82dd
bashforger <bash.forger@gmail.com>
2020-11-19 14:53:03 +0500

    Added my_each_with_index method

--- main.rb
+++ main.rb
@@ -1,11 +1,17 @@
 module Enumerable
   def my_each
     return for i in self do
-      yield i
+             yield i
     end
   end
 
   def my_each_with_index
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+    self
   end
 
   def my_select
@@ -33,5 +39,15 @@ module Enumerable
   end
 end
 
-puts ([3,2,1,4,5,6]).each { |i| } 
-puts ([3,2,1,4,5,6]).my_each { |i| }
+# # 1. my_each
+# puts 'my_each'
+# puts '-------'
+# puts [1, 2, 3].my_each { |elem| print "#{elem + 1} " } # => 2 3 4
+# puts
+
+# 2. my_each_with_index
+puts 'my_each_with_index'
+puts '------------------'
+print([1, 2, 3].each_with_index { |elem, idx| puts "#{elem} : #{idx}" }) # => 1 : 0, 2 : 1, 3 : 2
+print([1, 2, 3].my_each_with_index { |elem, idx| puts "#{elem} : #{idx}" }) # => 1 : 0, 2 : 1, 3 : 2
+puts
