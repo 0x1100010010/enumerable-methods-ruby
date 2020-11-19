bceaaae2d3bb9332980808cf199f9ed6bfa8660f
bashforger <bash.forger@gmail.com>
2020-11-19 17:22:43 +0500

    Finished 1st half of my_all

--- main.rb
+++ main.rb
@@ -22,7 +22,11 @@ module Enumerable
     arr
   end
 
-  def my_all?
+  def my_all?(*args)
+    return args unless block_given?
+
+    (self.my_select { |i| yield(i) }).length == self.length
+    # puts args # args.kind_of?(args) #(self.my_select { |i| i==args } )
   end
 
   def my_any?
@@ -57,10 +61,24 @@ end
 # print([1, 2, 3].my_each_with_index { |elem, idx| puts "#{elem} : #{idx}" }) # => 1 : 0, 2 : 1, 3 : 2
 # puts
 
-# 3. my_select
-puts 'my_select'
-puts '---------'
-p [1, 2, 3, 8].my_select(&:even?) # => [2, 8]
-p [0, 2018, 1994, -7].my_select { |n| n > 0 } # => [2018, 1994]
-p [6, 11, 13].my_select(&:odd?) # => [11, 13]
+# # 3. my_select
+# puts 'my_select'
+# puts '---------'
+# p [1, 2, 3, 8].my_select(&:even?) # => [2, 8]
+# p [0, 2018, 1994, -7].my_select { |n| n > 0 } # => [2018, 1994]
+# p [6, 11, 13].my_select(&:odd?) # => [11, 13]
+# puts
+
+# 4. my_all? (example test cases)
+puts 'my_all?'
+puts '-------'
+p [3, 5, 7, 11].my_all?(&:odd?) # => true
+p [-8, -9, -6].my_all? { |n| n < 0 } # => true
+p [3, 5, 8, 11].my_all?(&:odd?) # => false
+p [-8, -9, -6, 0].my_all? { |n| n < 0 } # => false
+# test cases required by tse reviewer
+p [1, 2, 3, 4, 5].my_all? # => true
+p [1, 2, 3].my_all?(Integer) # => true
+p %w[dog door rod blade].my_all?(/d/) # => true
+p [1, 1, 1].my_all?(1) # => true
 puts
