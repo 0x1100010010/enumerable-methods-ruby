c0b1e0991906f4061f0d2ed33c01cc5c0844ea4d
Andrik Solis <Andrik27001@gmail.com>
2020-12-10 18:43:40 +0100

    Add my_each method first test

--- spec/tests_rspec.rb
+++ spec/tests_rspec.rb
@@ -1,1 +1,11 @@
 require '../main.rb'
+
+describe Enumerable do
+  arr = [3, 4, 7, 1, 2]
+
+  describe '#my_each' do
+    it "returned each value" do
+      expect(arr.my_each {|i|} ).to eq(arr.each{|i|})
+    end
+  end
+end
